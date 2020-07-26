bb151bf277171fb1e4c84d160a77cbfbc698e27d
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-26 19:25:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,7 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts "It's-a me, Mario!"
 end
 
 def toadstool
-  puts status
+  puts 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
